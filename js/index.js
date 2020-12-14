4474f272a578afcbf16a73d7f78738eb2f41dd61
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2020-12-14 08:40:00 +0800

    109.12.13

---
+++ js/index.js
@@ -0,0 +1,87 @@
+let data = {
+    flyer_data: {
+        flyer_menu: [
+            {
+                flyer_id: 1,
+                flyer_name: "居仁國小",
+                flyer_src: "./images/flyer/01.jpg",
+                isActive: true,
+
+            },
+            {
+                flyer_id: 2,
+                flyer_name: "居仁國中",
+                flyer_src: "./images/flyer/02.jpg",
+                isActive: false,
+
+            },
+            {
+                flyer_id: 1,
+                flyer_name: "居仁國小",
+                flyer_src: "./images/flyer/01.jpg",
+                isActive: false,
+
+            },
+            {
+                flyer_id: 2,
+                flyer_name: "居仁國中",
+                flyer_src: "./images/flyer/02.jpg",
+                isActive: false,
+
+            },
+            {
+                flyer_id: 1,
+                flyer_name: "居仁國小",
+                flyer_src: "./images/flyer/01.jpg",
+                isActive: false,
+
+            },
+            {
+                flyer_id: 2,
+                flyer_name: "居仁國中",
+                flyer_src: "./images/flyer/02.jpg",
+                isActive: false,
+
+            },
+            {
+                flyer_id: 1,
+                flyer_name: "居仁國小",
+                flyer_src: "./images/flyer/01.jpg",
+                isActive: false,
+
+            },
+            {
+                flyer_id: 2,
+                flyer_name: "居仁國中",
+                flyer_src: "./images/flyer/02.jpg",
+                isActive: false,
+
+            },
+            {
+                flyer_id: 1,
+                flyer_name: "居仁國小",
+                flyer_src: "./images/flyer/01.jpg",
+                isActive: false,
+            },
+            {
+                flyer_id: 2,
+                flyer_name: "居仁國中",
+                flyer_src: "./images/flyer/02.jpg",
+                isActive: false,
+            },
+            {
+                flyer_id: 2,
+                flyer_name: "居仁國中",
+                flyer_src: "./images/flyer/02.jpg",
+                isActive: false,
+            },
+        ],
+        flyer_menu_start: 0,
+        flyer_menu_end: 0,
+        flyer_now_pic: 0,
+        flyer_menu_move: "",
+        flex_moveX: 0,
+        flyer_menu_iscenter:"justify-content: center"
+    },
+    menuActive: false,
+}
